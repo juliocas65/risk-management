5a92975b556311fc9c3c986e30aae1544a7c07b0
Julio Castillo <juliocastillo@MacBook-Pro-de-Julio.local>
2021-03-21 18:10:46 -0300

    fix travil yaml

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "$KUBE_CA_CERT" | base64 --decode > cert.crt
+
+/usr/local/bin/kubectl \
+  --kubeconfig=/dev/null \
+  --server=$KUBE_ENDPOINT \
+  --certificate-authority=cert.crt \
+  apply -f ./kubernetes/k8s-application-deployment.yml
+
+echo "The deploy is Ready"
