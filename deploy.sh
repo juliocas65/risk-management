7ee1dd2ad29ea70583860d912448baa96f6c6bb1
Julio Castillo <juliocastillo@MacBook-Pro-de-Julio.local>
2021-03-21 18:48:23 -0300

    fix travil yaml

--- deploy.sh
+++ deploy.sh
@@ -4,10 +4,9 @@ echo "$KUBE_CA_CERT" | base64 --decode > cert.crt
 echo "$KUBE_ENDPOINT"
 echo "$KUBE_USERNAME"
 
-/usr/local/bin/kubectl \
-  --kubeconfig=/dev/null \
+/usr/local/bin/kubectl  --kubeconfig=/dev/null \
   --server=$KUBE_ENDPOINT \
-  --certificate-authority=cert.crt \
+  --certificate-authority=$KUBE_CA_CERT \
   --username=$KUBE_USERNAME \
   apply -f kubernetes/k8s-application-deployment.yml
 
